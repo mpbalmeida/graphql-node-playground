753f9af519db70f4f8aa12f046854b3d03ec616c
Haider Malik <haidermalik504@gmail.com>
2018-07-10 15:15:51 +0500

    first query

--- src/index.js
+++ src/index.js
@@ -1,15 +1,57 @@
 const { GraphQLServer } = require('graphql-yoga');
 
+let links = [
+    {
+        id: 'link-0',
+        url: 'www.fullstackhour.com',
+        description: 'Fullstak web app with React, Node,Prisma and GraphQL'
+    },
+    {
+        id: 'link-1',
+        url: 'www.fullstackhour.com',
+        description: 'GraphQL advanced Course'
+    },
+    {
+        id: 'link-2',
+        url: 'www.fullstackhour.com',
+        description: 'PassportJs course'
+    },
+    {
+        id: 'link-3',
+        url: 'www.fullstackhour.com',
+        description: 'NodeJs course'
+    }
+]
 const typeDefs = `
 type Query {
  info: String!
+ feed : [Link!]!
+}
+type Link {
+    id: ID!
+    url : String!
+    description: String!
 }
 `;
 
 const resolvers = {
     Query: {
-        info: () => 'Hello GraphQL devs !!'
+        info: () => 'Hello GraphQL devs !!',
+        feed: () => links //first execution
+    },
+    Link: {
+        id(root) {
+            console.log(root)
+            return root.id;
+        },
+        url(root) {
+            return root.url
+        },
+        description(root) {
+            return root.description
+        }
     }
+
 }
 
 const server = new GraphQLServer({
